04ff94b0976a70bc88c920cd3951abe3dbf71e8a
dbreton333 <davidbreton03@gmail.com>
2023-12-06 18:51:22 -0500

    cleaned init

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,6 +1,5 @@
 from .agent import Agent
 from .random_agent import RandomAgent
 from .human_agent import HumanAgent
-from .emile_agent import EmileAgent
 from .student_agent import StudentAgent
 from .iterative_student_agent import IterativeStudentAgent
